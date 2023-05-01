52834818b70d93ba684e3967d9fd178e0aeff134
billalxcode <billal.xcode@gmail.com>
2023-05-01 15:07:49 +0700

    seo friendly

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -8,15 +8,25 @@ export default function Home() {
   return (
     <>
       <Helmet>
+        <title>PPDB SMKN 1 Maja</title>
         <meta name="title" content="PPDB SMKN 1 Maja" />
         <meta name="description" content="Selamat datang di Penerimaan Peserta Didik Baru Sekolah Menengah Kejuruan Negeri 1 Maja, Majalengka, Jawa Barat" />
-        <meta name="keywords" content="ppdb, ppdb2023, jabar, ppdb jabar, majalengka, smkn1maja, smk, smkppdb, ppdb smk" />
-        <meta name="robots" content="index, follow" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="Indonesian" />
         <meta name="author" content="Billal Fauzan" />
+        <meta name="keywords" content="ppdb, ppdb2023, jabar, ppdb jabar, majalengka, smkn1maja, smk, smkppdb, ppdb smk" />
 
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="PPDB SMKN 1 Maja" />
+        <meta property="og:description" content="Selamat datang di Penerimaan Peserta Didik Baru Sekolah Menengah Kejuruan Negeri 1 Maja, Majalengka, Jawa Barat" />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="PPDB SMKN 1 Maja" />
+        <meta property="twitter:description" content="Selamat datang di Penerimaan Peserta Didik Baru Sekolah Menengah Kejuruan Negeri 1 Maja, Majalengka, Jawa Barat" />
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
       </Helmet>
+      
       <div className="flex justify-center mt-5">
         <div className="card shadow w-9/12 lg:w-3/4">
           <div className="card-body">
