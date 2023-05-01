2185c008353be85bc0efa6e1951db22f108c6b32
billalxcode <billal.xcode@gmail.com>
2023-05-01 15:10:32 +0700

    split and save as a component

---
+++ src/components/seo.jsx
@@ -0,0 +1,25 @@
+import { Helmet } from "react-helmet"
+
+export default function SEO() {
+    return (
+        <Helmet>
+            <title>PPDB SMKN 1 Maja</title>
+            <meta name="title" content="PPDB SMKN 1 Maja" />
+            <meta name="description" content="Selamat datang di Penerimaan Peserta Didik Baru Sekolah Menengah Kejuruan Negeri 1 Maja, Majalengka, Jawa Barat" />
+            <meta name="author" content="Billal Fauzan" />
+            <meta name="keywords" content="ppdb, ppdb2023, jabar, ppdb jabar, majalengka, smkn1maja, smk, smkppdb, ppdb smk" />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://metatags.io/" />
+            <meta property="og:title" content="PPDB SMKN 1 Maja" />
+            <meta property="og:description" content="Selamat datang di Penerimaan Peserta Didik Baru Sekolah Menengah Kejuruan Negeri 1 Maja, Majalengka, Jawa Barat" />
+            <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://metatags.io/" />
+            <meta property="twitter:title" content="PPDB SMKN 1 Maja" />
+            <meta property="twitter:description" content="Selamat datang di Penerimaan Peserta Didik Baru Sekolah Menengah Kejuruan Negeri 1 Maja, Majalengka, Jawa Barat" />
+            <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        </Helmet>
+    )
+}
