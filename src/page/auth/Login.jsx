b3b05ef3f9b76ffb1d69a23d4b645d0dceb06807
billalxcode <billal.xcode@gmail.com>
2023-05-12 19:42:40 +0700

    add user login

--- src/page/auth/Login.jsx
+++ src/page/auth/Login.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import SEO from "../../components/seo"
 import authLogin from "../../service/auth"
-import { redirect } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { getSession, removeSession, saveSession } from "../../service/session"
 
 export default function Login() {
     let [ email, setEmail ] = useState('')
     let [ password, setPassword ] = useState('')
+    let [ logged, setLogged ] = useState(false)
     let [ alertMessage, setAlertMessage ] = useState({
 
     })
@@ -15,10 +17,17 @@ export default function Login() {
             try {
                 const response = await authLogin(email, password)
                 if (response.status === 200) {
-                    return redirect('/dashboard')
+                    let user = response.data.data
+                    saveSession('logged', true)
+                    saveSession('user', JSON.stringify(user))
+                    setAlertMessage({
+                        message: <Link to={'/dashboard'} className="cursor-auto">Login sukses, silahkan pergi ke <span className="text-white cursor-pointer">Dashboard</span></Link>,
+                        type: 'alert-success'
+                    })
+                    setLogged(true)
                 } else {
                     setAlertMessage({
-                        message: 'unknown error',
+                        message: 'Error! unknown error',
                         type: 'alert-error'
                     })
                 }
@@ -26,7 +35,7 @@ export default function Login() {
                 let response = error.response.data
                 let message = response.message
                 setAlertMessage({
-                    message: message,
+                    message: "Error! " + message,
                     type: 'alert-error'
                 })
             }
@@ -35,6 +44,21 @@ export default function Login() {
         event.preventDefault()
     }
 
+    useEffect(() => {
+        let user = getSession('user')
+        if (user !== undefined) {
+            let user_json = JSON.parse(user)
+            if (Object.keys(user_json).includes('access_token') && Object.keys(user_json).includes('expires_in')) {
+                setLogged(true)
+                setAlertMessage({
+                    message: <span>You are already logged</span>,
+                    type: 'alert-warning'
+                })
+            } else {
+                removeSession('user')
+            }
+        }
+    }, [])
     return (
         <>
             <SEO></SEO>
@@ -49,7 +73,7 @@ export default function Login() {
                                 <div className={"alert " + alertMessage.type + " shadow-lg"}>
                                     <div>
                                         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                                        <span>Error! { alertMessage.message }</span>
+                                        <span>{ alertMessage.message }</span>
                                     </div>
                                 </div>
                             : "" ) }
@@ -57,13 +81,13 @@ export default function Login() {
                                 <label className="label">
                                     <span className="label-text">Email:</span>
                                 </label>
-                                <input type="email" placeholder="Enter your email" className="input input-bordered w-full" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <input type="email" placeholder="Enter your email" className="input input-bordered w-full" value={email} onChange={(e) => setEmail(e.target.value)} disabled={logged} />
                             </div>
                             <div className="form-control w-full my-2">
                                 <label className="label">
                                     <span className="label-text">Password:</span>
                                 </label>
-                                <input type="password" placeholder="Enter your password" className="input input-bordered w-full" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                <input type="password" placeholder="Enter your password" className="input input-bordered w-full" value={password} onChange={(e) => setPassword(e.target.value)} disabled={logged} />
                             </div>
                             <button className="btn btn-primary w-full my-2" onClick={submit}>Submit</button>
                         </form>
