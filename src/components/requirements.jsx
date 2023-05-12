c83509b733166d135b2c687ba67ab4457292ca1a
billalxcode <billal.xcode@gmail.com>
2023-05-12 19:42:26 +0700

    fix error on console

--- src/components/requirements.jsx
+++ src/components/requirements.jsx
@@ -12,11 +12,11 @@ export default function Requirements() {
     ];
 
     return (
-        <ul class="mb-8 space-y-4 text-left text-gray-500 dark:text-gray-400">
+        <ul className="mb-8 space-y-4 text-left text-gray-500 dark:text-gray-400">
             { requirements.map((data, index) => {
                 return (
-                    <li class="flex items-center space-x-3">
-                        <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                    <li className="flex items-center space-x-3" key={index}>
+                        <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
                         <span>{ data }</span>
                     </li>
                 )
